e4d9ea10fb714e0a4096cdb87ad2ff5d5cb2beed
Makbook Air 15 <makbookair15@Makbooks-MacBook-Air.local>
2023-09-28 13:22:38 +0300

    created custom modal and show it after creation of deck or card

--- client/src/components/createDeck/CreateDeck.jsx
+++ client/src/components/createDeck/CreateDeck.jsx
@@ -1,32 +1,51 @@
-import React, { useState} from 'react';
-import { useSelector } from 'react-redux';
-import { createDeck } from '../../store/reducers/deckSlice';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { createDeck } from "../../store/reducers/deckSlice";
+import { useDispatch } from "react-redux";
+import CustomModal from "../customModal/CustomModal";
 
-import './CreateDeck.scss';
+import "./CreateDeck.scss";
 
 const CreateDeck = () => {
-    const user = useSelector((state) => state.auth.user);
-    const [deckName, setDeckName] = useState('');
-    const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+  const [showModal, setShowModal] = useState(false);
+  const [message, setMessage] = useState("");
+  const [deckName, setDeckName] = useState("");
+  const dispatch = useDispatch();
 
-    const handleCreateDeck = () => {
-        console.log('create deck', user);
-        dispatch(createDeck({ name: deckName, userId: user._id }));
-    }
+  const handleCreateDeck = () => {
+    dispatch(createDeck({ name: deckName, userId: user._id }))
+      .then((response) => {
+        setMessage("Deck Created");
+        setShowModal(true);
+        setDeckName("");
+      })
+      .catch((error) => {
+        console.error("Deck creation failed", error);
+      });
+  };
+
+
+  const handleClose = () => {
+    setShowModal(false);
+    setMessage("");
+  }
 
   return (
     <div>
-        <h1>Create Deck</h1>
-        <input
-            type='text'
-            placeholder='Deck Name'
-            value={deckName}
-            onChange={(e) => setDeckName(e.target.value)}
-        />
-        <button onClick={handleCreateDeck}>Create Deck</button>
+      <h1>Create Deck</h1>
+      <input
+        type="text"
+        placeholder="Deck Name"
+        value={deckName}
+        onChange={(e) => setDeckName(e.target.value)}
+      />
+      <button onClick={handleCreateDeck}>Create Deck</button>
+      {showModal && (
+        <CustomModal message={message} onClose={handleClose} />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default CreateDeck;
